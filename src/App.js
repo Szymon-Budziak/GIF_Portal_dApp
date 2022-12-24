e1fcc028e81ce9780e73ee60ba99b19131d01488
Szymon-Budziak <szymonbudziak@gmail.com>
2022-12-24 01:24:07 +0100

    GIF Portal dApp initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
 
-function App() {
+// Constants
+const TWITTER_HANDLE = "_buildspace";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    if (window?.solana?.isPhantom) {
+      console.log("Phantom wallet found!");
+      const response = await window.solana.connect({ onlyIfTrusted: true });
+      console.log("Connected with Public Key:", response.publicKey.toString());
+
+      setWalletAddress(response.publicKey.toString());
+    } else {
+      alert("Solana object not found! Get a Phantom Wallet 👻");
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={walletAddress ? "authed-container" : "container"}>
+        <div className="header-container">
+          <p className="header">🖼 GIF Portal</p>
+          <p className="sub-text">View your GIF collection in the metaverse ✨</p>
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
